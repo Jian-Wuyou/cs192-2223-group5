b37c42b16333411ca93ec61b35ce7a9d6c614dba
Jian-Wuyou <dramante@up.edu.ph>
2023-03-24 20:03:33 +0800

    feat: add API access point for linking UVLe

--- lms_hub/views/__init__.py
+++ lms_hub/views/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 from .login import login
 from .logout import logout
 from .dashboard import dashboard
+from .link_lms.uvle import UVLe
 
 def init_views(app: Flask):
     app.register_blueprint(login)
     app.register_blueprint(logout)
     app.register_blueprint(dashboard)
+    app.register_blueprint(UVLe)
---
+++ lms_hub/views/link_lms/uvle.py
@@ -0,0 +1,48 @@
+from os import environ
+
+from flask import Blueprint, current_app, render_template, request
+from flask_login import current_user, login_required
+from lms_hub.models.credentials import UVLeCredentials
+
+import requests
+
+UVLe = Blueprint("link-uvle", __name__)
+
+@UVLe.route("/link/uvle", methods=["GET"])
+@login_required
+def login_page():
+    return render_template("login-uvle.html")
+
+@UVLe.route("/link/uvle", methods=["POST"])
+@login_required
+def login_post():
+    plaintext_creds = request.json["credentials"]
+    username, password = plaintext_creds.split(":")
+    domain = environ["UVLE_DOMAIN"]
+    url = f"https://{domain}/login/token.php"
+    payload = {
+        "username": username,
+        "password": password,
+        "service": "moodle_mobile_app",
+    }
+
+    r = requests.get(url, params=payload)
+    if not 200 <= r.status_code <= 299:
+        # Unsuccessful request
+        # Could not connect to server, etc
+        return '{"success": false, "error": "Could not connect to the server"}', 400
+
+    json = r.json()
+    if "token" not in json or len(json["token"]) < 32:
+        return '{"success": false, "error": "Invalid credentials"}', 400
+
+    creds = UVLeCredentials(
+        username = username,
+        token = json["token"],
+        server = domain
+    )
+
+    db = current_app.config["DB"]
+    db.update_user_lms_creds("uvle", current_user.user_id, creds)
+
+    return '{"success": true}'
