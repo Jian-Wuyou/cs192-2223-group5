03f513aef08af2629b9cd8ef80cc608fbdcc54dd
Jian-Wuyou <dramante@up.edu.ph>
2023-03-24 22:23:40 +0800

    feat: add unlinking for UVLe

--- lms_hub/views/link_lms/uvle.py
+++ lms_hub/views/link_lms/uvle.py
@@ -1,6 +1,6 @@
 from os import environ
 
-from flask import Blueprint, current_app, render_template, request
+from flask import Blueprint, current_app, render_template, request, redirect, url_for
 from flask_login import current_user, login_required
 from lms_hub.models.credentials import UVLeCredentials
 
@@ -12,6 +12,13 @@ UVLe = Blueprint("link-uvle", __name__)
 @login_required
 def login_page():
     return render_template("lms/login-uvle.html")
+
+@UVLe.route("/unlink/uvle")
+@login_required
+def logout_post():
+    db = current_app.config["DB"]
+    db.delete_user_lms_creds("uvle", current_user.user_id)
+    return redirect(url_for('dashboard.dashboard_page'))
 
 @UVLe.route("/link/uvle", methods=["POST"])
 @login_required
@@ -26,11 +33,15 @@ def login_post():
         "service": "moodle_mobile_app",
     }
 
-    r = requests.get(url, params=payload)
+    try:
+        r = requests.get(url, params=payload)
+    except requests.RequestException as e:
+        return '{"success": false, "error": "Could not connect to the LMS server"}', 400
+
     if not 200 <= r.status_code <= 299:
         # Unsuccessful request
         # Could not connect to server, etc
-        return '{"success": false, "error": "Could not connect to the server"}', 400
+        return '{"success": false, "error": "Could not connect to the LMS server"}', 400
 
     json = r.json()
     if "token" not in json or len(json["token"]) < 32:
