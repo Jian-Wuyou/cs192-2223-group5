86fa637a2e4e14838c71f47065408426e9660f52
ArcticIcicle <cjnchoa@gmail.com>
2023-05-25 18:25:11 +0800

    dashboard ui updates

--- templates/lms/dashboard_script/dashboard.js
+++ templates/lms/dashboard_script/dashboard.js
@@ -1,47 +1,80 @@
 function addCard(assignment){
     const box = document.getElementById('deadlines');
 
+
     const newcard = document.createElement('div');
-    newcard.className = 'p-2 bg-white shadow-sm d-flex align-items-center rounded border border-dark border-3';
+    newcard.className = 'classcard bg-light m-2 h-100 rounded';
+    newcard.style = 'max-width:300px';
     newcard.setAttribute('id', assignment.course_name);
 
-    const newp = document.createElement('p');
-    newp.className = 'fs-2 m-2'
-    newp.textContent = assignment.course_name;
+    const newcardheader = document.createElement('div');
+    newcardheader.className = 'classcard-header text-center';
+    newcardheader.textContent = assignment.course_name;
 
-    const newi = document.createElement('i');
-    newi.className = 'fas fa-briefcase fs-1 primary-text m-2'
-    newi.setAttribute('id', 'briefCaseLogo');
+    const newcardbody = document.createElement('div');
+    newcardbody.className = 'classcard-body';
 
-    const newassignment = document.createElement('bold');
-    newassignment.className = "fs-4 m-2";
-    newassignment.textContent = assignment.name
+    const newassignmentTitle = document.createElement('h5');
+    newassignmentTitle.className = "classcard-title";
+    newassignmentTitle.textContent = assignment.name;
+
+    const newassignmentDesc = document.createElement('p');
+    newassignmentDesc.className = "classcard-text scroll";
+    newassignmentDesc.textContent = assignment.description;
+
+    const newcardfooter = document.createElement('div');
+    newcardfooter.className = 'classcard-footer text-center bg-light';
+
+
+    const newassignmentLink = document.createElement('a');
+    newassignmentLink.className = "btn btn-primary";
+    newassignmentLink.href = assignment.course_url;
+    newassignmentLink.target = "_blank"
+    newassignmentLink.textContent = "Go to page";
 
     box.appendChild(newcard);
-    newcard.appendChild(newp);
-    newcard.appendChild(newi);
-    newcard.appendChild(newassignment);
+
+    newcard.appendChild(newcardheader);
+    newcard.appendChild(newcardbody);
+    newcard.appendChild(newcardfooter);
+    newcardbody.appendChild(newassignmentTitle);
+    newcardbody.appendChild(newassignmentDesc);
+    newcardfooter.appendChild(newassignmentLink);
 }
 
 function numberOfAssignments(n){
     const box = document.getElementById('assignmentNumber');
-    box.textContent = n + " Assignments Due!"
+    const text1 = document.createElement('span');
+    text1.style = "color:#fff";
+    text1.textContent = "You Have ";
+    box.appendChild(text1)
+    const text2 = document.createElement('span');
+    text2.style = "color:#ff0000";
+    text2.className = "redtext";
+    text2.textContent = n;
+    box.appendChild(text2)
+    const text3 = document.createElement('span');
+    text3.style = "color:#fff";
+    text3.textContent = " Assigments Due!";
+    box.appendChild(text3)
 }
 
 function ownerGreeting(n){
     const box = document.getElementById('owner');
-    var greetingsArray = ["Welcome back, ", "Let's get you back from where you left off, ", "Good day, ", "Back to work, "]
-    var greeting = greetingsArray[Math.floor(Math.random()*greetingsArray.length)]
-    box.textContent = greeting + n + "!"
+    var greetingsArray = ["Welcome back, ", "Let's get you back from where you left off, ", "Good day, ", "Back to work, "];
+    var greeting = greetingsArray[Math.floor(Math.random()*greetingsArray.length)];
+    box.textContent = greeting + n + "!";
 }
 
 fetch('dashboard_script/deadlines.json')
     .then(response => response.json())
     .then((json) => {
-        ownerGreeting("John")
-        numberOfAssignments(json.deadlines.length)
+        ownerGreeting("John");
+        numberOfAssignments(json.deadlines.length);
         for (var i = 0; i < json.deadlines.length; i++){
-            console.log(json.deadlines[i])
-            addCard(json.deadlines[i])
+            console.log(json.deadlines[i]);
+            addCard(json.deadlines[i]);
         }
     });
+
+
